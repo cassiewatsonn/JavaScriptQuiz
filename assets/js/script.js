5aa4085f645887c848faeb57926e1509e08ad105
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-01-24 22:44:41 -0500

    added index.html, script.js and started quiz first page

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//Start the Game 
+function startGame() {
+
+}
+
+//DOM Variables 
+let startButton = document.querySelector('#startBtn');
+
+//Question Variables
+let correct = 0; 
+
+
+let quizQuestions = [{
+question: " ", 
+answerChoices: [" ", " ", ],
+correct: 
+}
+
+
+
+
+]
