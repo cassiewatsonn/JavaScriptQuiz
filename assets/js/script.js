74bd5f0f5c2d82d87a6ac3ea553715acea8945d1
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-01-25 20:30:20 -0500

    added quis questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
 //Start the Game 
 function startGame() {
-    timeLeft(); 
-    showQuestion(); 
+    timeLeft();
+    showQuestion();
 }
 
-function showQuestion(){
-    let quizQuestions = document.createElement("h2"); 
-        quizQuestions.textContent = [thisQuestion].question; 
+function showQuestion() {
+    let quizQuestions = document.createElement("h2");
+    quizQuestions.textContent = [thisQuestion].question;
 
 }
 
-function timeLeft(){
+function timeLeft() {
 
 }
 
 //Click the START! button -> Event Listener
-startButton.addEventListener("click"), function(event){
-    event.stopPropagation(); 
-    startButton.disabled = true; 
+startButton.addEventListener("click"), function (event) {
+    event.stopPropagation();
+    startButton.disabled = true;
     startGame();
 }
 
@@ -25,16 +25,27 @@ startButton.addEventListener("click"), function(event){
 let startButton = document.querySelector('#startBtn');
 
 //Question Variables
-let correct = 0; 
-let thisQuestion = 0; 
-let timeLeft = 150; 
+let correct = 0;
+let thisQuestion = 0;
+let timeLeft = 150;
 
-
-let quizQuestions = [{
-question: "What data types are supported by Javascript? a: boolean and string, b: symbol and number, c:null and undefined", 
-answerChoices: ["a", "all of the above", "c" ],
-correct: 1; 
-}
+//Quiz Question Array//
+let quizQuestions = [
+    {
+        question: "What data types are supported by Javascript? a: boolean and string, b: symbol and number, c:null and undefined",
+        answerChoices: ["a", "b", "all of the above", "c"],
+        correct: 2,
+    },
+    {
+        question: "What HTML element do we put JavaScript?",
+        answerChoices: ["<javascript>", "<script>", "<link>", "<js"],
+        correct: 1,
+    },
+    {
+        question: "Where is the correct place to insert JavaScript?",
+        answerChoices: ["the bottom of the <body>", "the <head>", "the top of the <body>", "the <header>"],
+        correct: 0,
+    },
 
 
 
